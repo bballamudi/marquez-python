9e5c19b2a04b70577fed61aace7db92fecba3a40
Rodrigo Araya <rodrigo.araya@gmail.com>
2020-01-30 11:57:49 -0800

    move make_field() to utils.py
    Signed-off-by: Rodrigo Araya <rodrigo.araya@gmail.com>

--- marquez_client/client.py
+++ marquez_client/client.py
@@ -310,25 +310,6 @@ class MarquezClient(object):
             }
         )
 
-    @staticmethod
-    def make_field(name, data_type, description=None):
-        if isinstance(data_type, str):
-            if not DatasetFieldType.__members__.__contains__(data_type):
-                raise ValueError(f'Invalid field type: {data_type}')
-        elif isinstance(data_type, DatasetFieldType):
-            data_type = data_type.name
-        else:
-            raise ValueError('data_type must be a str or a DatasetFieldType')
-
-        DatasetType.__members__.get(data_type)
-        field = {
-            'name': name,
-            'type': data_type
-        }
-        if description:
-            field['description'] = description
-        return field
-
     # Common
 
     def _url(self, path, *args):
